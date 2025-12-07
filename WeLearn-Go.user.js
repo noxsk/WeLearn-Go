8c1d08a7fe167995fe1103fa7d48ad49c83b9a02
Noxsk <108687061+noxsk@users.noreply.github.com>
2025-12-08 01:21:58 +0800

    Bump version to 0.9.1

--- WeLearn-Go.user.js
+++ WeLearn-Go.user.js
@@ -2,7 +2,7 @@
 // @name         WeLearn-Go
 // @namespace    https://github.com/noxsk/WeLearn-Go
 // @supportURL   https://github.com/noxsk/WeLearn-Go/issues
-// @version      0.9.0
+// @version      0.9.1
 // @description  自动填写 WeLearn 练习答案，支持小错误生成、自动提交和批量任务执行！
 // @author       Noxsk
 // @match        https://welearn.sflep.com/*
